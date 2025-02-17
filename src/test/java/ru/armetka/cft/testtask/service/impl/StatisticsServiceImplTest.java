582167938967d9b9761e14836261d2b5328273c4
ArMetka <artem4fight@gmail.com>
2025-02-17 21:51:31 +0700

    add tests for statistics service

--- src/test/java/ru/armetka/cft/testtask/service/impl/StatisticsServiceImplTest.java
+++ src/test/java/ru/armetka/cft/testtask/service/impl/StatisticsServiceImplTest.java
@@ -12,6 +12,7 @@ import ru.armetka.cft.testtask.storage.impl.ParsedDataStorageImpl;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,10 +37,7 @@ class StatisticsServiceImplTest {
     @Test
     public void itCountsShort() {
         this.intList.add(new BigInteger("0"));
-        this.intList.add(new BigInteger("0"));
 
-        this.floatList.add(new BigDecimal("0"));
-        this.floatList.add(new BigDecimal("0"));
         this.floatList.add(new BigDecimal("0"));
         this.floatList.add(new BigDecimal("0"));
 
@@ -49,17 +47,88 @@ class StatisticsServiceImplTest {
 
         Statistics stat = this.statisticsService.generateStatistics(StatisticsModesEnum.SHORT);
 
-        Assertions.assertEquals(2, stat.getIntCount());
-        Assertions.assertEquals(4, stat.getFloatCount());
+        Assertions.assertEquals(1, stat.getIntCount());
+        Assertions.assertEquals(2, stat.getFloatCount());
         Assertions.assertEquals(3, stat.getStringCount());
     }
 
     @Test
-    public void itHandlesZeroCount() {
+    public void itHandlesZeroCountShort() {
         Statistics stat = this.statisticsService.generateStatistics(StatisticsModesEnum.SHORT);
 
         Assertions.assertEquals(0, stat.getIntCount());
         Assertions.assertEquals(0, stat.getFloatCount());
         Assertions.assertEquals(0, stat.getStringCount());
+    }
+
+    @Test
+    public void itCountsFull() {
+        this.intList.add(new BigInteger("0"));
+
+        this.floatList.add(new BigDecimal("0"));
+        this.floatList.add(new BigDecimal("0"));
+
+        this.stringList.add("0");
+        this.stringList.add("0");
+        this.stringList.add("0");
+
+        Statistics stat = this.statisticsService.generateStatistics(StatisticsModesEnum.FULL);
+
+        Assertions.assertEquals(1, stat.getIntCount());
+        Assertions.assertEquals(2, stat.getFloatCount());
+        Assertions.assertEquals(3, stat.getStringCount());
+    }
+
+    @Test
+    public void itHandlesZeroCountFull() {
+        Statistics stat = this.statisticsService.generateStatistics(StatisticsModesEnum.FULL);
+
+        Assertions.assertEquals(0, stat.getIntCount());
+        Assertions.assertEquals(new BigInteger("0"), stat.getIntMin());
+        Assertions.assertEquals(new BigInteger("0"), stat.getIntMax());
+        Assertions.assertEquals(new BigInteger("0"), stat.getIntSum());
+        Assertions.assertEquals(new BigDecimal("0"), stat.getIntAvg());
+
+        Assertions.assertEquals(0, stat.getFloatCount());
+        Assertions.assertEquals(new BigDecimal("0"), stat.getFloatMin());
+        Assertions.assertEquals(new BigDecimal("0"), stat.getFloatMax());
+        Assertions.assertEquals(new BigDecimal("0"), stat.getFloatSum());
+        Assertions.assertEquals(new BigDecimal("0"), stat.getFloatAvg());
+
+        Assertions.assertEquals(0, stat.getStringCount());
+        Assertions.assertEquals(0, stat.getStringLenMin());
+        Assertions.assertEquals(0, stat.getStringLenMax());
+    }
+
+    @Test
+    public void itCalculatesFull() {
+        this.intList.add(new BigInteger("1234567891234567891234567890"));
+        this.intList.add(new BigInteger("-1234567891234567891234567890"));
+        this.intList.add(new BigInteger("50"));
+
+        this.floatList.add(new BigDecimal("1234567891234567891234567890"));
+        this.floatList.add(new BigDecimal("-1234567891234567891234567890"));
+        this.floatList.add(new BigDecimal("-50"));
+
+        this.stringList.add("very long string с русскими буквами!!!");
+        this.stringList.add("この世の不利益はすべて当人の能力不足。");
+
+        Statistics stat = this.statisticsService.generateStatistics(StatisticsModesEnum.FULL);
+
+        Assertions.assertEquals(3, stat.getIntCount());
+        Assertions.assertEquals(new BigInteger("-1234567891234567891234567890"), stat.getIntMin());
+        Assertions.assertEquals(new BigInteger("1234567891234567891234567890"), stat.getIntMax());
+        Assertions.assertEquals(new BigInteger("50"), stat.getIntSum());
+        Assertions.assertEquals(new BigDecimal("50").divide(new BigDecimal("3"), 4, RoundingMode.HALF_UP), stat.getIntAvg());
+
+        Assertions.assertEquals(3, stat.getFloatCount());
+        Assertions.assertEquals(new BigDecimal("-1234567891234567891234567890"), stat.getFloatMin());
+        Assertions.assertEquals(new BigDecimal("1234567891234567891234567890"), stat.getFloatMax());
+        Assertions.assertEquals(new BigDecimal("-50").setScale(4, RoundingMode.HALF_UP), stat.getFloatSum());
+        Assertions.assertEquals(new BigDecimal("-50").divide(new BigDecimal("3"), 4, RoundingMode.HALF_UP), stat.getFloatAvg());
+
+        Assertions.assertEquals(2, stat.getStringCount());
+        Assertions.assertEquals(19, stat.getStringLenMin());
+        Assertions.assertEquals(38, stat.getStringLenMax());
     }
 }
